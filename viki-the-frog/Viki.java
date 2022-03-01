e470884caf9838cb761ff2f76e342063c2b98928
USER <sasankavirajsva@gmail.com>
2022-03-01 13:41:45 +0530

    Viki the frog

---
+++ viki-the-frog/Viki.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Viki{
+
+	public static void main(String[] args){
+		System.out.println("How far Viki needs to jump?");
+		Scanner scanner = new Scanner(System.in);
+		int distance = scanner.nextInt();
+		int seconds = 0;
+		while(distance > 0){
+			if(distance >= 5){
+				distance -= 5;
+				if(distance != 0){
+					seconds += 1;
+					distance -= 3;
+					if(distance != 0){
+						seconds += 2;
+						distance -= 1;
+						if(distance!=0){
+							seconds += 3;
+						}
+					}
+				}
+			}else{
+				distance = 0;
+			}
+		}
+		System.out.println("Viki takes "+seconds+" seconds to jump");
+	}
+}
