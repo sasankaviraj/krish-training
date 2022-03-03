c362bb57e65bb41efce31c398c1a7fc630e4d1f6
USER <sasankavirajsva@gmail.com>
2022-03-04 01:25:44 +0530

    singleton example

--- singleton-sample/src/main/java/Configuration.java
+++ singleton-sample/src/main/java/Configuration.java
@@ -7,6 +7,12 @@ public class Configuration {
     public static volatile Properties properties;
     private static final String FILE_NAME = "/application.properties";
 
+    private Configuration(){
+        if (null == properties){
+            throw new RuntimeException("Use getPropertiesInstance method to get a new instance");
+        }
+    }
+
     public static Properties getPropertiesInstance(){
         if (null == properties){
             synchronized (Configuration.class){
